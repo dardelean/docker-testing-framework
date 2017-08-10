7e8919bbc957bac66ff7d5eaa750f6e8d8217ca8
Dan Ardelean <dardelean@cloudbasesolutions.com>
2017-08-10 13:04:27 -0700

    First commit

---
+++ DockerUtils.psm1
@@ -0,0 +1,60 @@
+function Start-ExternalCommand {
+    <#
+    .SYNOPSIS
+    Helper function to execute a script block and throw an exception in case of error.
+    .PARAMETER ScriptBlock
+    Script block to execute
+    .PARAMETER ArgumentList
+    A list of parameters to pass to Invoke-Command
+    .PARAMETER ErrorMessage
+    Optional error message. This will become part of the exception message we throw in case of an error.
+    #>
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$true)]
+        [Alias("Command")]
+        [ScriptBlock]$ScriptBlock,
+        [array]$ArgumentList=@(),
+        [string]$ErrorMessage
+    )
+    PROCESS {
+        if($LASTEXITCODE){
+            # Leftover exit code. Some other process failed, and this
+            # function was called before it was resolved.
+            # There is no way to determine if the ScriptBlock contains
+            # a powershell commandlet or a native application. So we clear out
+            # the LASTEXITCODE variable before we execute. By this time, the value of
+            # the variable is not to be trusted for error detection anyway.
+            $LASTEXITCODE = ""
+        }
+        $res = Invoke-Command -ScriptBlock $ScriptBlock -ArgumentList $ArgumentList
+        if ($LASTEXITCODE) {
+            if(!$ErrorMessage){
+                Throw ("Command exited with status: {0}" -f $LASTEXITCODE)
+            }
+            throw ("{0} (Exit code: $LASTEXITCODE)" -f $ErrorMessage)
+        }
+        return $res
+    }
+}
+
+function Write-DebugMessage {
+    <#
+    .SYNOPSIS
+    Helper function to write a message if debug is enabled.
+    .PARAMETER isDebug
+    If this is true, write the debug message, if not, don't
+    .PARAMETER Message
+    Message to be outputed
+    #>
+    [CmdletBinding()]
+    Param
+    (
+        [string]$isDebug,
+        [string]$Message
+    )
+
+        if ($isDebug -eq 'yes') { Write-Output "`n$Message`n" }
+}
+
+Export-ModuleMember -Function * -Alias *
---
+++ tests.ps1
@@ -0,0 +1,238 @@
+# Script that tests Docker on Windows functionality
+Param(
+    [string]$isDebug='no' 
+)
+
+$ErrorActionPreference = "Stop"
+$WORK_PATH = Split-Path -parent $MyInvocation.MyCommand.Definition
+$CONFIGS_PATH = $WORK_PATH + "\configs\"
+$CONTAINER_NAME = "container1"
+$CONTAINER_IMAGE = "nginx"
+$CONTAINER_PORT = 80
+$NODE_PORT = 8080
+$VOLUME_NAME = "vol1"
+$NETWORK_NAME = "net1"
+
+Import-Module "$WORK_PATH\DockerUtils"
+
+function New-Container {
+    Param(
+        [string]$containerName,
+        [string]$containerImage,
+        [int]$nodePort,
+        [int]$containerPort,
+        [string]$configPath
+    )
+
+    Start-ExternalCommand -ScriptBlock { docker pull $containerImage } `
+    -ErrorMessage "`nFailed to pull docker image`n"
+
+    # Fire a container with a shared volume and a web server listening on 80
+    Start-ExternalCommand -ScriptBlock { docker run -d -p $nodePort`:$containerPort `
+    -v $configPath`:/data --name $containerName $containerImage } `
+    -ErrorMessage "`nError creating container`n"
+
+    Write-DebugMessage $isDebug -Message "Container created SUCCSESSFULLY"
+
+    $containerID = docker container inspect $containerName --format "{{.ID}}"
+    return $containerID
+}
+
+function New-Volume([string]$volumeName) {
+    Start-ExternalCommand -ScriptBlock { docker volume create $volumeName } `
+    -ErrorMessage "`nFailed to create docker volume with $LastExitCode`n"
+
+    Write-DebugMessage $isDebug -Message "Volume created SUCCSESSFULLY"
+
+    # docker does not asign an ID to volume so cannot return one
+}
+
+function New-Image([string]$imageName) {
+    Start-ExternalCommand -ScriptBlock { docker pull $imageName } `
+    -ErrorMessage "`nFailed to pull docker image with $LastExitCode`n"
+
+    Write-DebugMessage $isDebug -Message "Image pulled SUCCSESSFULLY"
+
+    $imageID = docker images $imageName --format "{{.ID}}"
+    return $imageID
+}
+
+function New-Network([string]$networkName) {
+    Start-ExternalCommand -ScriptBlock { docker network create $networkName } `
+    -ErrorMessage "`nFailed to create network with $LastExitCode`n"
+
+    Write-DebugMessage $isDebug -Message "Network created SUCCSESSFULLY"
+
+    $networkID = docker network inspect $networkName --format "{{.ID}}"
+    return $networkID
+}
+
+function Get-HTTPGet {
+    # Check if the container responds on 8080
+    $res = Invoke-WebRequest -Uri http://localhost:8080
+    if ($res.StatusCode -gt 400) {
+        throw "`nContainer did NOT respond to HTTP GET`n"
+        exit
+    } else {
+        Write-DebugMessage $isDebug -Message "Container responded to HTTP GET SUCCSESSFULLY"
+    }
+}
+
+function Get-Attribute {
+    # get the attributes of a container, network, volume
+        Param
+    (
+        [ValidateSet("container", "network", "volume", "image")]
+        [string]$elementType,
+        [string]$elementName,
+        [string]$attribute
+    )
+
+    $attribute = Start-ExternalCommand -ScriptBlock { docker $elementType inspect `
+    $elementName --format "{{.$attribute}}"} `
+    -ErrorMessage "`nCould not get attributes of $elementType $elementName`n"
+
+    return $attribute
+}
+
+function Get-SharedVolume([string]$containerName) {
+    # Check if data in the shared volume is accessible 
+    # from containers mountpoint
+    $volumeData = docker exec $containerName ls /data
+    if(!$volumeData) {
+        throw "`nCannot access shared volume`n"
+        
+    } else {
+        Write-DebugMessage $isDebug -Message "Container shared volume accessed SUCCSESSFULLY"
+    }
+}
+
+function Get-Command([string]$containerName) {
+    # Check if a command can be succsessfully run in a container
+    Start-ExternalCommand -ScriptBlock { docker exec $containerName ls } `
+    -ErrorMessage "`nFailed to exec command with $LastExitCode`n"
+
+    Write-DebugMessage $isDebug -Message "Exec runned SUCCSESSFULLY"
+}
+
+function Connect-Network([string]$networkName, [string]$containerName) {
+    New-Network $networkName
+
+    Start-ExternalCommand -ScriptBlock { docker network connect $networkName $containerName } `
+    -ErrorMessage "`nFailed to connect network to container with $LastExitCode`n "
+
+    Write-DebugMessage $isDebug -Message "Network conneted SUCCSESSFULLY"
+}
+
+function Clear-Environment {
+    # Delete existing containers, volumes or images if any
+    if($(docker ps -a -q).count -ne 0) {
+        docker stop $(docker ps -a -q)
+        docker rm $(docker ps -a -q)  
+    }
+
+    if($(docker volume list -q).count -ne 0) {
+        docker volume rm $(docker volume list -q)
+    }
+
+    if ($(docker images -a -q).count -ne 0) {
+        docker rmi -f (docker images -a -q)
+    }
+
+    docker network prune --force
+}
+
+function Test-Restart([string]$containerName) {
+    # Restart container and see if all the functionalities
+    # are available
+    Start-ExternalCommand -ScriptBlock { docker restart $containerName } `
+    -ErrorMessage "`nFailed to restart container`n"
+
+    Get-HTTPGet
+    Get-SharedVolume $containerName
+
+    Write-DebugMessage $isDebug -Message "Restart container tests ran SUCCSESSFULLY"
+}
+
+function Test-Building {
+    Param(
+        [string]$containerName,
+        [string]$containerImage,
+        [int]$nodePort,
+        [int]$containerPort,
+        [string]$configPath
+    )
+
+    (Get-Content "$configPath\Dockerfile").replace('image', $containerImage) `
+    | Set-Content "$configPath\Dockerfile"
+
+    Start-ExternalCommand -ScriptBlock { docker build -f "$configPath\Dockerfile" -t testingimage . } `
+    -ErrorMessage "`nFailed to build docker image`n"
+    
+    Start-ExternalCommand -ScriptBlock { docker run -d -p $nodePort`:$containerPort -v `
+    $configPath`:/data --name $containerName testingimage } `
+    -ErrorMessage "`nFailed to start container`n"
+
+    docker stop $containerName
+    docker rm $containerName
+
+    Write-DebugMessage $isDebug -Message "Container built SUCCSESSFULLY"
+}
+
+function Test-BasicFunctionality {
+    Param
+    (
+        [string]$volumeName,
+        [string]$imageName,
+        [string]$networkName,
+        [string]$containerName
+    )
+    Write-Output "`n============Starting functionality tests===============`n"
+    
+    # Run the functionalities tests, no containers yet
+    New-Volume $volumeName
+    New-Image $imageName
+    New-Network $networkName
+
+    Clear-Environment
+
+    Write-Output "`n============Functionality tests PASSED===============`n"
+}
+
+function Test-BasicContainers {
+    Param(
+        [string]$containerName,
+        [string]$containerImage,
+        [int]$nodePort,
+        [int]$containerPort,
+        [string]$configPath,
+        [string]$networkName
+    )
+
+    Write-Output "`n============Starting create container tests===============`n"
+
+    Test-Building $containerName $containerImage $nodePort $containerPort $configPath
+    # Create container and test all the functionalities on it 
+    New-Container $containerName $containerImage $nodePort $containerPort $configPath
+ 
+    # Execute functionality tests
+    Get-Command $containerName
+    Get-HTTPGet
+    Get-SharedVolume $containerName
+    Test-Restart $containerName
+    Connect-Network $networkName $containerName
+
+    #$created = Get-Attribute container $containerName Created
+    #Write-Output $created
+
+    Write-Output "`n============Create container tests PASSED===============`n"
+
+    # Cleanup container
+    Clear-Environment
+}
+
+Clear-Environment
+Test-BasicFunctionality $VOLUME_NAME $CONTAINER_IMAGE $NETWORK_NAME $CONTAINER_NAME
+Test-BasicContainers $CONTAINER_NAME $CONTAINER_IMAGE $NODE_PORT $CONTAINER_PORT $CONFIGS_PATH $NETWORK_NAME
+
+Write-Output "`n============All tests PASSED===============`n"
